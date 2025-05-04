5b34292e9ba10695dfa91ec511bd7d5832db4c99
Kishan0101 <kp9792709@gmail.com>
2025-05-05 01:08:15 +0530

    git commits

--- backend/server.js
+++ backend/server.js
@@ -34,11 +34,9 @@ try {
 }
 
 // MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
+
+
   .then(() => console.log('MongoDB connected'))
   .catch((err) => {
     console.error('MongoDB connection error:', err.message);
